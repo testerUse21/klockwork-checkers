10cf3c58592dab6bf259b37954a2477b77a9d146
AparnaSGhenge <43434515+AparnaSGhenge@users.noreply.github.com>
2022-03-02 14:52:31 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,14 @@
+void iterate(int n){
+     int i;
+     for (i = 0; i < n; i++){
+       foo();
+     }
+  
+   }
+   void iterateFoo()
+   {
+    unsigned num;
+    scanf("%u",&num);
+ 
+    iterate(num);
+   }
