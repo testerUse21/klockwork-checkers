144bbb2bda9ae7b970600b1eff9d58a11919f9a2
testerUse21 <106081205+testerUse21@users.noreply.github.com>
2022-07-04 15:43:39 +0530

    Create cert-str34.c

---
+++ cert-str34.c
@@ -0,0 +1,15 @@
+
+static int yy_string_get() {
+  register char *string;
+  register int c;
+ 
+  string = bash_input.location.string;
+  c = EOF;
+ 
+  /* If the string doesn't exist, or is empty, EOF found. */
+  if (string && *string) {
+    c = *string++;
+    bash_input.location.string = string;
+  }
+  return (c);
+}
